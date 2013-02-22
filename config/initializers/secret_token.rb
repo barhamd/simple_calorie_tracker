6e3d250b3a52f2c873405e52ec11612c087310f3
Devin <devinbarham@gmail.com>
2013-02-21 23:51:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCalorieTracker::Application.config.secret_token = '277cdcb2ade09fcf785ef1bb289d2e9faf560ca3a5ad774ff1c6e4b32711122aa8d34ace538e79bdcadf0531cd0f65275b84ab488407533562103d71c4e60255'
