c124f5dce237f7c16d93852d0589a3fe149af39a
Devin <devinbarham@gmail.com>
2013-02-25 22:23:19 -0500

    Add basic home and help static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns a full title if no page title is present
+  def full_title(page_title)
+    base_title = "Simple Calorie Tracker"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
