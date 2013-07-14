57da94b281274d87123833d3c1506ee75329d646
Devin Barham <barhamd@gmail.com>
2013-07-14 16:16:13 -0400

    Moved logic from view to model. Maybe helps clear up things?

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,4 +1,12 @@
 class Tracker < ActiveRecord::Base
   attr_accessible :calories, :consumed_on, :user_id
   belongs_to :user
+
+  def date_and_calories
+      date = consumed_on.to_time.to_i*1000
+      [date,calories]
+  end
+
+  def sum_by_day
+  end
 end
