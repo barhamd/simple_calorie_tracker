714294d1331bcfd2d84ccd67258c14d118468c32
Devin Barham <barhamd@gmail.com>
2014-03-06 00:35:19 -0500

    Move logic out of view. Update home page to only show 10 most recent calorie logs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-     @calorie_log = CalorieLog.new
+     @new_calorie_log = CalorieLog.new
+     @calorie_logs = current_user.calorie_logs.order('consumed_on DESC').limit(10)
     end
   end
 
