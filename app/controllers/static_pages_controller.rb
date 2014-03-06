caa72c916b5253e6167e1491e8e425fbbd1b1d9e
Devin Barham <barhamd@gmail.com>
2014-03-06 01:11:07 -0500

    Display time uniformly. Display 1 months worth of logs.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
      @new_calorie_log = CalorieLog.new
-     @calorie_logs = current_user.calorie_logs.order('consumed_on DESC').limit(10)
+     @calorie_logs = current_user.calorie_logs.where('consumed_on > ?', Date.today - 1.month).order('consumed_on DESC')
     end
   end
 
